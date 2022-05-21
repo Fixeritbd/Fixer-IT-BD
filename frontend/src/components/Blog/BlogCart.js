b7e687b5d9b2649bf415544956cf8817c891aac9
shahinooriact <shahinooriact@gmail.com>
2022-05-21 20:28:32 +0600

    Our Blog complete by shahinoor

--- frontend/src/components/Blog/BlogCart.js
+++ frontend/src/components/Blog/BlogCart.js
@@ -3,25 +3,27 @@ import blogcard from "./blogcard.png";
 import "./Blog.css";
 import { Button, Card, Col, Nav, Row } from "react-bootstrap";
 import { BiTime } from "react-icons/bi";
+import { IoMdArrowRoundForward } from "react-icons/io";
 
 const BlogCart = () => {
   return (
     <div>
-      <div className="cards">
-        <Card>
-          <Card.Img variant="top" src={blogcard} />
+      <div >
+        <Card className="cards">
+          <Card.Img variant="top" src={blogcard} className="card_img"/>
           <Card.Body>
             <div className="time">
               <span>
                 <BiTime />
-                22 March, 2022
+              <span className="px-2">22 March, 2022</span>  
               </span>{" "}
+              
               <Button className="btn_marketing">Marketing</Button>
             </div>
             <Card.Title>
-              <h4>Post launch shower initiative forward</h4>
+              <h4 className="py-3">Post launch shower initiative forward</h4>
             </Card.Title>
-            <a href="#">Continue Reading...</a> ...
+            <a href="#">Continue Reading <IoMdArrowRoundForward/></a> 
           </Card.Body>
         </Card>
       </div>

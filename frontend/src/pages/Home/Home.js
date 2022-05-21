c52ca453a673feb30c89fc51537d6ac0ee50b32f
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2022-05-21 15:24:08 +0600

    pricing part running

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import AboutPart from "../../components/About/AboutPart";
 import Banner from "../../components/Banner/Banner";
 import Featured from "../../components/Featured/Featured";
+import Pricing from "../../components/Pricing/Pricing";
 import Service from "../../components/Service/Service";
 import Startup from "../../components/Startup/Startup";
 import VideoPart from "../../components/Video Part/Video"
@@ -14,7 +15,8 @@ function Home() {
       <Service />
       <Featured />
       <Startup />
-    <VideoPart/>
+      <VideoPart />
+      <Pricing />
     </div>
   );
 }
